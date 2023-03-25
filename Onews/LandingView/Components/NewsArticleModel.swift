ccbdc51f0d29d02473731c62426eb1ea40b7937d
Sizwe Khathi <sizwek@globalkinetic.com>
2023-03-25 16:37:50 +0200

    Add landing view model
    Add LandingViewModel
    Add Test Data

--- Onews/LandingView/Components/NewsArticleModel.swift
+++ Onews/LandingView/Components/NewsArticleModel.swift
@@ -2,31 +2,15 @@
 //  NewsArticleModel.swift
 //  Onews
 //
-//  Created by Sizwe Khathi on 2023/03/24.
+//  Created by Sizwe Khathi on 2023/03/25.
 //
 
 import Foundation
 
-// MARK: - ApiRequestOutcome
-struct ApiRequestOutcome: Codable {
-    let status: String
-    let totalResults: Int
-    let articles: [NewsArticleModel]
-}
-
-// MARK: - NewsArticleModel
-struct NewsArticleModel: Codable {
-    let source: Source
-    let author: String?
-    let title, description: String
-    let url: String
-    let urlToImage: String
-    let publishedAt: Date
-    let content: String?
-}
-
-// MARK: - Source
-struct Source: Codable {
-    let id: String?
-    let name: String
+struct NewsArticleModel {
+    
+    let articleTitle, articleDescription, articleURL, articleImgURL: String
+    let articleAuthor, articleContent: String?
+    let publishedDate: Date
+    let articleSource: Source
 }
