50c4b16de6b460ba392fcc13dd69a11b5572ad79
sizwek <sizwek@globalkinetic.com>
2024-03-23 12:49:00 +0200

    Remove duplicate guard

--- Onews/LandingView/ViewController/LandingViewController+TableView.swift
+++ Onews/LandingView/ViewController/LandingViewController+TableView.swift
@@ -56,8 +56,6 @@ extension LandingViewController: UITableViewDelegate, UITableViewDataSource {
             view.removeBlurFromView()
             return cell
         } else {
-            guard let mainArticle = LandingViewModel.shared.mainArticle else { return UITableViewCell() }
-            
             guard let cell = tableView.dequeueReusableCell(withIdentifier: MainArticleTableViewCell.identifier) as? MainArticleTableViewCell else { return UITableViewCell() }
             
             cell.mainArticleView.articlesArray = Array(LandingViewModel.shared.articlesArray.prefix(3))
