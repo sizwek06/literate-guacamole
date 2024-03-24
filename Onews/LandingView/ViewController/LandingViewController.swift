bc2b160ede78f11377fd257efbbf9aedede57e53
sizwek <szwe031@gmail.com>
2024-03-24 14:08:31 +0200

    Move the landing view delegate

--- Onews/LandingView/ViewController/LandingViewController.swift
+++ Onews/LandingView/ViewController/LandingViewController.swift
@@ -91,29 +91,3 @@ extension Date {
         return ""
     }
 }
-
-// MARK: Articles Manager Delegate
-extension LandingViewController: ArticleDelegate {
-    
-    func reloadNewsArticles() {
-        articlesManager.fetchNewsArticles()
-        tableView.reloadData()
-    }
-    
-    func didReceiveArticlesSuccessfully() {
-        tableView.reloadData()
-    }
-    
-    func didFailWithError(error: String) {
-        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Retry", style: UIAlertAction.Style.default, handler: { (_) in
-            self.reloadNewsArticles()
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: { (_) in
-            self.dismiss(animated: true)
-        }))
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-}
