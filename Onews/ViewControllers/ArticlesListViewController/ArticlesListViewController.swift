a9936c71d1a4ff8ba015de3f25aaf75e79d3aa44
Sizwe Khathi <szwe031@gmail.com>
2024-04-10 13:42:27 +0200

    Update project for integration to sdk

--- Onews/ViewControllers/ArticlesListViewController/ArticlesListViewController.swift
+++ Onews/ViewControllers/ArticlesListViewController/ArticlesListViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import Kingfisher
+import OnewsSDK
 
 class ArticlesListViewController: BaseTableViewController {
     
--- Onews/ViewControllers/BaseTableViewController.swift
+++ Onews/ViewControllers/BaseTableViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import FirebaseFirestore
+import OnewsSDK
 
 class BaseTableViewController: UIViewController {
     
